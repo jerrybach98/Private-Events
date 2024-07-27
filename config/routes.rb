6881bfed058cc4874f2118acbee5525fa27bc9ae
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-07-27 03:37:45 -0700

    Set up devise User and responder gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events, only: [:new, :create, :index]
 
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "events#index"
 end
