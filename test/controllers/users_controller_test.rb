415609fc78510fb0f46ce48ba2b250d957f67638
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-08-02 01:58:48 -0700

    Route, controller, and view creation for showing user events

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get users_home_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get users_index_url
+    assert_response :success
+  end
+end
