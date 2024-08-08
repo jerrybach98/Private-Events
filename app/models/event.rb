0a19b466e69181d2ecad69c5ca70fd0c4305c80b
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-08-08 03:31:14 -0700

    Navigation links and separate past events
    Root page has button to user's events and links to events. Past events are separated from upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: 'user_id'
   has_many :event_registrations, dependent: :destroy
   has_many :attendees, :through => :event_registrations, source: :user
+
+  scope :upcoming, -> { where('date > ?', DateTime.now) }
+  scope :past, -> { where('date < ?', DateTime.now) }
+
 end
